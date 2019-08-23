beea61474603ec2df9d0c2fdacaa009e30d8ab8f
Ning_L <1320710158@qq.com>
2019-08-23 20:57:06 +0800

    堆排序

---
+++ data_structure/heapsort.c
@@ -0,0 +1,86 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2019 LingYun<lingyun@email.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  heapsort.c
+ *    Description:  堆排序
+ *                 
+ *        Version:  1.0.0(22/08/19)
+ *         Author:  LingYun <lingyun@email.com>
+ *      ChangeLog:  1, Release initial version on "22/08/19 16:04:58"
+ *                 
+ ********************************************************************************/
+#include <stdio.h>
+
+//交换值
+void swap( int *i, int *j )
+{
+    int     temp;
+
+    temp = *i;
+    *i = *j;
+    *j = temp;
+}
+
+//判断双亲结点和子节点的大小
+void HeapAdjust( int array[], int i, int len )
+{
+    //i表示双亲结点，j表示子结点
+
+    int     j=i*2+1;
+
+    while( j<len )
+    {
+        if( j+1<len && array[j]<array[j+1] )
+        {
+            j++;
+        }
+
+        if( array[i]>array[j] )
+        {
+            break;
+        }
+        else
+        swap( &array[i],&array[j]);
+
+        i = j;
+        j = 2*i+1;
+    }
+}
+
+void MakeHeap( int array[],int n )
+{
+    int     i;
+    for( i=n/2-1; i>=0; i-- )
+    {
+        HeapAdjust( array, i, n );
+    }
+}
+
+
+void HeapSort( int array[], int len )
+{
+    int     i=0;
+    MakeHeap( array, len );
+
+    for(i=len-1; i>=0; i--)
+    {
+        swap( &array[i], &array[0] );
+        HeapAdjust( array, 0, i );
+    }
+}
+
+void main()
+{
+    int i,a[9]={5,2,6,0,3,9,1,7,4};
+
+    HeapSort(a,9);
+
+    printf("排序后的结果是：");
+
+    for( i=0; i<9; i++ )
+    {
+        printf("%d",a[i]);
+    }
+    printf("\n");
+}
